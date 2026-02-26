15b4d1ec75f67b743edd0c8bd0e6fceeb0d35388
ColeIsaacHarris <cole@coleharris.net>
2026-02-26 15:28:50 -0700

    Fix DashboardTest to use Pest syntax and remove unused variable

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -1,26 +1,13 @@
 <?php
 
 use App\Models\User;
-use Tests\TestCase;
 
-(new class extends TestCase {
-    public function test_guests_are_redirected_to_the_login_page(): void
-    {
-        $this->get(route('dashboard'))->assertRedirect(route('login'));
-    }
-
-    public function test_authenticated_users_can_visit_the_dashboard(): void
-    {
-        $this->actingAs($user = User::factory()->create());
-
-        $this->get(route('dashboard'))->assertOk();
-    }
-})->test_guests_are_redirected_to_the_login_page();
+test('guests are redirected to the login page', function () {
     $this->get(route('dashboard'))->assertRedirect(route('login'));
 });
 
 test('authenticated users can visit the dashboard', function () {
-    $this->actingAs($user = User::factory()->create());
+    $this->actingAs(User::factory()->create());
 
     $this->get(route('dashboard'))->assertOk();
 });
