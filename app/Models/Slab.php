9f680f0e005235be2d6d2c2ca5e9596eabdef7c0
ColeIsaacHarris <cole@coleharris.net>
2026-03-04 22:30:17 -0700

    Update Slab model class

--- app/Models/Slab.php
+++ app/Models/Slab.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Slab extends Model
 {
-    //
+    /** @use HasFactory<\Database\Factories\SlabFactory> */
     use HasFactory;
+
+    /** @var array<string> */
+    protected $fillable = [
+    ];
 }
