34c4639d60e05df15ff236d4ba2c315a62047070
ColeIsaacHarris <cole@coleharris.net>
2026-03-19 14:51:32 -0600

    Add and configure Filament package

--- config/auth.php
+++ config/auth.php
@@ -41,7 +41,7 @@ return [
             'provider' => 'users',
         ],
         'admin' => [
-            'driver' => 'sessions',
+            'driver' => 'session',
             'provider' => 'admins',
         ],
     ],
